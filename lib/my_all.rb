5388ec9d63d2f91fb61316c9dbb33c1dfb270518
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 00:40:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,11 +7,14 @@ def my_all?(collection)
     return_values << yield(collection[i])
     i += 1
   end
+
+  if return_values.include?(false)
+    false
+  else
+    true
+  end
+  
 end
 
-if return_values.include?(false)
-  false
-else
-  true
-end
+
   
